603151880fd0f255435446e1aa8552c216c4f363
Josh Cawthorne <joshcawthorne97@gmail.com>
2021-07-31 14:46:08 +0100

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    PUBLIC_SUPABASE_URL: process.env.PUBLIC_SUPABASE_URL,
+    PUBLIC_SUPABASE_ANON_KEY: process.env.PUBLIC_SUPABASE_ANON_KEY,
+  },
+};
