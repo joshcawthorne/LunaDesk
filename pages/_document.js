42aec6ebb6004a659f6f95e72a418479e3231c25
Josh Cawthorne <joshcawthorne97@gmail.com>
2021-09-15 12:48:50 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import Document, { Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
